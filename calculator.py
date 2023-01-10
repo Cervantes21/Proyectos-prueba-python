b1b4d5d7ee93d149362782e4bc1c1dc237eacd26
cervantes21 <admon.owlsoul@gmail.com>
2023-01-10 01:10:52 -0600

    Update

--- calculator.py
+++ calculator.py
@@ -8,14 +8,47 @@ class Calculator():
 
     def sumar(self):
         suma = self.a + self.b
-        print (f'Tú resultado es: {suma}')
+        print (f'Tu resultado es: {suma}')
+
+    def restar(self):
+        resta = self.a - self.b
+        print (f'Tu resultado es: {resta}')
+        
+    def multiplicar(self):
+        multiplication = self.a * self.b
+        print (f'Tu resultado es: {multiplication}')
+
+    def dividir(self):
+        division = self.a / self.b
+        print (f'Tu resultado es: {multiplication}')
 
 if __name__ == '__main__':
     pass
 
-option = str(input('Escoge un operador \n 1. Sumar + \n 2. Restar - \n 3. Multiplicar *  \n 4. Dividir / \n'))
-        if option == '+':
-            a = input('Ingresa un número: ')
-            b = input('Ingrese otro número: ')
-            calculator = Calculator(a,b)
-            calculator.sumar()
+option = input('Escoge un operador \n 1. Sumar + \n 2. Restar - \n 3. Multiplicar *  \n 4. Dividir / \n')
+if option == '+':
+    a = input('Ingresa un número: ')
+    b = input('Ingrese otro número: ')
+    calculator = Calculator(a,b)
+    calculator.sumar()
+
+elif option == '-':
+    a = input('Ingresa un número: ')
+    b = input('Ingrese otro número: ')
+    calculator = Calculator(a,b)
+    calculator.restar()
+
+elif option == '*':
+    a = input('Ingresa un número: ')
+    b = input('Ingrese otro número: ')
+    calculator = Calculator(a,b)
+    calculator.multiplicar()
+
+elif option == '/':
+    a = input('Ingresa un número: ')
+    b = input('Ingrese otro número: ')
+    calculator = Calculator(a,b)
+    calculator.dividir()
+
+else:
+    print ('Opción incorrecta, utiliza un símbolo de operador como +, -, *, /')
