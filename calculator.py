2f2e0af9d00237c862a12767a2a68116f54bf747
cervantes21 <admon.owlsoul@gmail.com>
2023-01-09 18:34:29 -0600

    Calculator OOP

---
+++ calculator.py
@@ -0,0 +1,21 @@
+class Calculator():
+    a = 0
+    b = 0
+
+    def __init__(self, a,b):
+        self.a = float(a)
+        self.b = float(b)
+
+    def sumar(self):
+        suma = self.a + self.b
+        print (f'Tú resultado es: {suma}')
+
+if __name__ == '__main__':
+    pass
+
+option = str(input('Escoge un operador \n 1. Sumar + \n 2. Restar - \n 3. Multiplicar *  \n 4. Dividir / \n'))
+        if option == '+':
+            a = input('Ingresa un número: ')
+            b = input('Ingrese otro número: ')
+            calculator = Calculator(a,b)
+            calculator.sumar()
