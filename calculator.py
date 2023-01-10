c8c1894c6899149b603a341ea4e87e8e0d5aad13
cervantes21 <admon.owlsoul@gmail.com>
2023-01-10 05:34:32 -0600

    V1_Calculator_OOP

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import os
+
 class Calculator():
     a = 0
     b = 0
@@ -19,36 +21,49 @@ class Calculator():
         print (f'Tu resultado es: {multiplication}')
 
     def dividir(self):
-        division = self.a / self.b
-        print (f'Tu resultado es: {multiplication}')
+        while True:
+            try:
+                division = self.a / self.b
+                print (f'Tu resultado es: {division}')
+            except ZeroDivisionError:
+                os.system('clear')
+                print ('No se puede dividir entre 0')
+                break
 
 if __name__ == '__main__':
     pass
+while True:
+    option = input('Escoge un operador \n 1. Sumar + \n 2. Restar - \n 3. Multiplicar *  \n 4. Dividir / \n Salir: Z \n')
+    os.system('clear')
+    if option == '+':
+        a = input('Ingresa un número: ')
+        b = input('Ingrese otro número: ')
+        calculator = Calculator(a,b)
+        calculator.sumar()
 
-option = input('Escoge un operador \n 1. Sumar + \n 2. Restar - \n 3. Multiplicar *  \n 4. Dividir / \n')
-if option == '+':
-    a = input('Ingresa un número: ')
-    b = input('Ingrese otro número: ')
-    calculator = Calculator(a,b)
-    calculator.sumar()
+    elif option == '-':
+        a = input('Ingresa un número: ')
+        b = input('Ingrese otro número: ')
+        calculator = Calculator(a,b)
+        calculator.restar()
 
-elif option == '-':
-    a = input('Ingresa un número: ')
-    b = input('Ingrese otro número: ')
-    calculator = Calculator(a,b)
-    calculator.restar()
+    elif option == '*':
+        a = input('Ingresa un número: ')
+        b = input('Ingrese otro número: ')
+        calculator = Calculator(a,b)
+        calculator.multiplicar()
 
-elif option == '*':
-    a = input('Ingresa un número: ')
-    b = input('Ingrese otro número: ')
-    calculator = Calculator(a,b)
-    calculator.multiplicar()
+    elif option == '/':
+        a = input('Ingresa un número: ')
+        b = input('Ingrese otro número: ')
+        calculator = Calculator(a,b)
+        calculator.dividir()
 
-elif option == '/':
-    a = input('Ingresa un número: ')
-    b = input('Ingrese otro número: ')
-    calculator = Calculator(a,b)
-    calculator.dividir()
+    elif option == 'z':
+        print('Au revoir!!')
+        break
 
-else:
-    print ('Opción incorrecta, utiliza un símbolo de operador como +, -, *, /')
+    else:
+        os.system('clear')
+        print ('Opción incorrecta \nutiliza un símbolo de operador como \n+, -, *, /\n')
+        continue
